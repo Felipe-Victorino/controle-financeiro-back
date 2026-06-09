8f1f2bd28e9eeb92f0583c5aac7712e5397894bd
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-06-09 11:21:27 -0300

    feat(entity):Created User Entity

--- src/main/java/com/finfin/backend/entity/User.java
+++ src/main/java/com/finfin/backend/entity/User.java
@@ -1,5 +1,35 @@
-package com.finfin.backend.model;
+package com.finfin.backend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
 public class User {
-  
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String hashedPassword;
+
+    private LocalDateTime createdIn = LocalDateTime.now();
+
+    private LocalDateTime updatedIn;
+
+
 }
