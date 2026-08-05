3812f2e2029b6ca98252fb30fd270d55fdb906ff
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-08-05 12:50:40 -0300

    feat( PasswordRecoveryTokenRepository.java ): find token by token query

--- src/main/java/com/finfin/backend/repository/PasswordRecoveryTokenRepository.java
+++ src/main/java/com/finfin/backend/repository/PasswordRecoveryTokenRepository.java
@@ -2,6 +2,14 @@ package com.finfin.backend.repository;
 
 import com.finfin.backend.entity.PasswordRecoveryToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.UUID;
 
 public interface PasswordRecoveryTokenRepository extends JpaRepository<PasswordRecoveryToken, Long> {
+
+    @Query("SELECT password_recovery_token from password_recovery_token prt where prt.token = ?1")
+    public PasswordRecoveryToken getPasswordRecoveryTokenByToken(UUID token);
+
+    PasswordRecoveryToken findByToken(UUID token);
 }
