35870874cc244d7a1ebda4ddccc1f74b8f59f294
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-08-05 12:53:32 -0300

    feat( UserService.java ): basic  email functionality

--- src/main/java/com/finfin/backend/service/UserService.java
+++ src/main/java/com/finfin/backend/service/UserService.java
@@ -2,7 +2,10 @@ package com.finfin.backend.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
+import com.finfin.backend.dto.auth.forgotpassword.ForgotDTORequest;
+import jakarta.validation.constraints.Email;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeMap;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +14,16 @@ import org.springframework.stereotype.Service;
 import com.finfin.backend.dto.auth.register.RegisterDTORequest;
 import com.finfin.backend.entity.User;
 import com.finfin.backend.repository.UserRepository;
+import org.thymeleaf.context.Context;
 
 @Service
 public class UserService{
 
     @Autowired
     private UserRepository repository;
+
+    @Autowired
+    private EmailSenderService emailSenderService;
 
     @Autowired
     private ModelMapper modelMapper;
@@ -26,19 +33,41 @@ public class UserService{
         return repository.findById(id).orElseThrow(()->new RuntimeException("{user.notfound}"));
     }
 
-
-    public User insert (RegisterDTORequest user){
-        TypeMap<RegisterDTORequest, User> propertyMapper = this.modelMapper.createTypeMap(RegisterDTORequest.class, User.class);
-        //addMapping recebe uma fonte e um destino, portanto o primeiro argumento é um getter e o segundo um setter
-        propertyMapper.addMapping(RegisterDTORequest::getPasswd, User::setHashedPassword);
-        return repository.save(this.modelMapper.map(user, User.class));
+    public User findByEmail(@Email String email){
+        return repository.findByEmail(email);
     }
+
+    public User insert (RegisterDTORequest userRequest){
+        TypeMap<RegisterDTORequest, User> propertyMapper = this.modelMapper.createTypeMap(RegisterDTORequest.class, User.class);
+
+        //addMapping recebe uma fonte e um destino, portanto o primeiro argumento é um getter e o segundo um setter
+
+        propertyMapper.addMapping(RegisterDTORequest::getPasswd, User::setHashedPassword);
+        User user = this.modelMapper.map(userRequest, User.class);
+
+        repository.save(user);
+
+//        Context context = new Context();
+//        context.setVariable("name", user.getName());
+//
+//        emailSenderService.sendTemplatedEmail(
+//                user.getEmail(),
+//                "Novo registro no serviço FinFin",
+//                "successfulRegistration",
+//                context
+//        );
+
+        return user;
+    }
+
+    public boolean confirmPassword(String passwd, String passwdConfirm){
+        return passwd.equals(passwdConfirm);
+    }
+
 
 
     public User update(User user){
         User userdb = findById(user.getId());
-        // TODO: address
-
         userdb.setAddress(user.getAddress());
         userdb.setName(user.getName());
         userdb.setUpdatedIn(LocalDateTime.now());
@@ -59,15 +88,10 @@ public class UserService{
         return repository.save(userdb);
     }
 
-
-
     public void delete(Long id) {
         User user = findById(id);
         repository.delete(user);
-
-
     }
-
 
     public List<User> listAll (){
         return repository.findAll();
