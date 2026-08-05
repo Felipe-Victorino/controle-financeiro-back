1759557ac1865794e3814e7db0aa5a0bff64a6de
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-08-05 12:50:16 -0300

    feat( UserRepository.java ): find user by email query

--- src/main/java/com/finfin/backend/repository/UserRepository.java
+++ src/main/java/com/finfin/backend/repository/UserRepository.java
@@ -2,6 +2,10 @@ package com.finfin.backend.repository;
 
 import com.finfin.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM user u WHERE u.email = ?1")
+    public User findByEmail(String email);
 }
