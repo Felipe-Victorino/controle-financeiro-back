a8f0cd91047d7127936d3fff534a2b3db329bf0c
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-08-05 12:54:07 -0300

    feat( UserController.java ): User Controller IS barebones, but better han nothing

--- src/main/java/com/finfin/backend/controller/UserController.java
+++ src/main/java/com/finfin/backend/controller/UserController.java
@@ -1,26 +1,39 @@
 package com.finfin.backend.controller;
 
-import com.finfin.backend.entity.User;
-import com.finfin.backend.service.UserService;
-import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.finfin.backend.dto.auth.register.RegisterDTORequest;
+import com.finfin.backend.entity.User;
+import com.finfin.backend.service.UserService;
+
+import jakarta.validation.Valid;
+
+
 @RestController
-@RequestMapping()
+@RequestMapping("/users")
+@CrossOrigin
 public class UserController {
+    
     @Autowired
     private UserService service;
 
-    @PostMapping("/auth/register")
-    public ResponseEntity<User> register(@RequestBody @Valid User user){
+    @PostMapping
+    public ResponseEntity<User> register(@RequestBody @Valid RegisterDTORequest user){
         User userdb = service.insert(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(userdb);
     }
+
+    @GetMapping()
+    public String defaultMessage(){return "User Controller";}
+    
   
 }
