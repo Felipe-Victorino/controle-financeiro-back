ae841a7853711c055a58f2a904b33cfb3896b2af
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-08-21 17:18:51 -0300

    fix ( CreateCategoryDTORequest.java, CategoryDTOResponse.java ): Requisição e Resposta para as cateogries

---
+++ src/main/java/com/finfin/backend/dto/CategoryDTOResponse.java
@@ -0,0 +1,37 @@
+package com.finfin.backend.dto;
+
+import com.finfin.backend.entity.enums.TransactionType;
+import jakarta.persistence.EnumeratedValue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CategoryDTOResponse {
+
+    private Long id;
+
+    @NotBlank(message ="{category.name.needed}")
+    @Size(max=80)
+    private String name;
+
+    @Pattern(
+            //traduzindo: ou um valor formatado desse jeito -> #FFF ou desse outro -> #FF00FF
+            regexp = "^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$",
+            message = "Valor deve ser uma cor válida"
+    )
+    private String color;
+
+
+    private String icon;
+
+    @NotBlank
+    @EnumeratedValue
+    @Pattern(regexp = "RECEIPT|EXPENSE")
+    private TransactionType type;
+
+    private boolean isActive;
+}
---
+++ src/main/java/com/finfin/backend/dto/CreateCategoryDTORequest.java
@@ -0,0 +1,35 @@
+package com.finfin.backend.dto;
+
+import com.finfin.backend.entity.enums.TransactionType;
+import jakarta.persistence.EnumeratedValue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CreateCategoryDTORequest {
+    private Long owner_id;
+
+    @NotBlank(message ="{category.name.needed}")
+    @Size(max=80)
+    private String name;
+
+
+    @Pattern(
+            //traduzindo: ou um valor formatado desse jeito -> #FFF ou desse outro -> #FF00FF
+            regexp = "^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$",
+            message = "Valor deve ser uma cor válida"
+    )
+    private String color;
+
+
+    private String icon;
+
+    @NotBlank
+    @EnumeratedValue
+    @Pattern(regexp = "RECEIPT|EXPENSE")
+    private TransactionType type;
+}
