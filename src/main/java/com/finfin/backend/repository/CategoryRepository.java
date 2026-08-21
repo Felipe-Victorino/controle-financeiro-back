a38a7cbb9c6c50be5a89af6490155bb492c02df0
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-08-21 17:14:54 -0300

    fix ( CategoryRepository.java ): Query para selecionar as categories só daquele usuário

--- src/main/java/com/finfin/backend/repository/CategoryRepository.java
+++ src/main/java/com/finfin/backend/repository/CategoryRepository.java
@@ -1,7 +1,14 @@
 package com.finfin.backend.repository;
 
 import com.finfin.backend.entity.Category;
+import com.finfin.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Query("select c from category c where c.owner = ?1")
+    List<Category> getCategoriesByOwner(Long id);
 }
