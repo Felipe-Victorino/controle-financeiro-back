6d9400e91b5a07921d1c7fa487cfd93337812818
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-09-03 14:17:01 -0300

    feat ( UserController.java ): Added to login DTO the name of the logged in person

--- src/main/java/com/finfin/backend/dto/auth/login/LoginDTOResponse.java
+++ src/main/java/com/finfin/backend/dto/auth/login/LoginDTOResponse.java
@@ -1,6 +1,7 @@
 package com.finfin.backend.dto.auth.login;
 
 import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,6 +11,10 @@ import java.time.LocalDateTime;
 public class LoginDTOResponse {
     //private JpaToken accessToken;
     //private TokenType;
+
+    @NotBlank
+    String name;
+
     @Future
     LocalDateTime expiresIn = LocalDateTime.now().plusHours(3);
 }
