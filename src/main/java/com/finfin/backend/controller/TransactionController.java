001384d7cb82856b9215ce6433d83ab5f4703419
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-09-03 14:13:13 -0300

    feat ( controller ): Created empty controller mappings for wallet, transaction and wallet member

---
+++ src/main/java/com/finfin/backend/controller/TransactionController.java
@@ -0,0 +1,11 @@
+package com.finfin.backend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+@CrossOrigin
+public class TransactionController {
+}
