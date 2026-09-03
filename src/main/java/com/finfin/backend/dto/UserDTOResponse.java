88adf6b746513cf2f1b0b3673c4945b7dad970cd
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-09-03 14:13:38 -0300

    feat ( controller ): Genereic DTO response for user interactions

---
+++ src/main/java/com/finfin/backend/dto/UserDTOResponse.java
@@ -0,0 +1,30 @@
+package com.finfin.backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class UserDTOResponse {
+
+    private Long id;
+
+    @NotBlank(message = "{user.name.needed}")
+    @Size(min=2, max=100)
+    private String name;
+
+    @NotBlank
+    @Email(message = "{user.email.needed}")
+    private String email;
+
+    @PastOrPresent
+    private LocalDateTime createdIn;
+
+    @PastOrPresent
+    private LocalDateTime updatedIn;
+}
