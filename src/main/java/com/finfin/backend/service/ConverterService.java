e70b480b6d43ebb46a75a9f49f780267ff58cfe3
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-09-03 14:12:17 -0300

    feat ( ConverterService.java ): Created a service that converts ids to objects by querying hte database, for use with the model mapper api

---
+++ src/main/java/com/finfin/backend/service/ConverterService.java
@@ -0,0 +1,61 @@
+package com.finfin.backend.service;
+import com.finfin.backend.dto.CategoryDTOResponse;
+import com.finfin.backend.entity.Transaction;
+import com.finfin.backend.entity.User;
+import com.finfin.backend.entity.WalletMember;
+import org.modelmapper.Converter;
+import com.finfin.backend.entity.Category;
+
+import org.modelmapper.spi.MappingContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConverterService {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TransactionService transactionService;
+
+    public Converter<Long, Category> idToCategoryConverter = new Converter<>() {
+        @Override
+        public Category convert(MappingContext<Long, Category> context) {
+            if (context.getSource() == null) {
+                return null;
+            }
+
+            return categoryService.findById(context.getSource());
+        }
+    };
+
+    public Converter<Long, Transaction> idToTransactionConverter = new Converter<>() {
+        @Override
+        public Transaction convert(MappingContext<Long, Transaction> context) {
+            if (context.getSource() == null) {
+                return null;
+            }
+
+            return transactionService.findById(context.getSource());
+        }
+    };
+
+    public Converter<Long, User> idToUserConverter = new Converter<Long, User>() {
+        @Override
+        public User convert(MappingContext<Long, User> context) {
+            if(context.getSource() == null){
+                return null;
+            }
+
+            return userService.findById(context.getSource());
+        }
+    };
+
+
+
+}
