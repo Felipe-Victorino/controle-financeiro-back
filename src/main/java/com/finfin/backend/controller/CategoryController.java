61c7f2e593b6ab3ed420f8b138894844ab0bcbb4
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-09-03 14:14:31 -0300

    feat ( CategoryController.java ): Added modelmapper mappings and the Put and Delete verbs

--- src/main/java/com/finfin/backend/controller/CategoryController.java
+++ src/main/java/com/finfin/backend/controller/CategoryController.java
@@ -1,14 +1,16 @@
 package com.finfin.backend.controller;
 
-import com.finfin.backend.dto.CreateCategoryDTORequest;
+import com.finfin.backend.dto.CategoryDTORequest;
 import com.finfin.backend.dto.CategoryDTOResponse;
 import com.finfin.backend.entity.Category;
 import com.finfin.backend.service.CategoryService;
 import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,37 +23,59 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/category")
-@CrossOrigin(origins = "localhost:5173")
+@CrossOrigin
 public class CategoryController {
 
     @Autowired
     CategoryService service;
 
+    @Autowired
+    ModelMapper mapper;
+
     @GetMapping("/{id}")
     public ResponseEntity<List<CategoryDTOResponse>> getAllFromUser(@PathVariable Long id){
 
-        List<CategoryDTOResponse> response = service.listAllByOwner(id);
+        List<Category> categoryList = service.listAllByOwner(id);
+
+        List<CategoryDTOResponse> response = categoryList.stream()
+                .map(category -> mapper.map(category, CategoryDTOResponse.class))
+                .toList();
+
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/")
-    public ResponseEntity<CategoryDTOResponse> createNewCategory(@RequestBody @Valid CreateCategoryDTORequest request){
+    public ResponseEntity<CategoryDTOResponse> createNewCategory(@RequestBody @Valid CategoryDTORequest request){
 
-        Category cat = service.createNewCategoryFromRequest(request);
+        Category cat = service.insert(this.mapper.map(request, Category.class));
 
-        CategoryDTOResponse response = new CategoryDTOResponse(
-                cat.getId(),
-                cat.getName(),
-                cat.getColor(),
-                cat.getIcon(),
-                cat.getType(),
-                cat.isActive()
-        );
+        CategoryDTOResponse response = this.mapper.map(cat, CategoryDTOResponse.class);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long id){
+
+        service.delete(id);
+
+        return ResponseEntity.ok().body("Categoria removida");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryDTOResponse> updateCategory(
+            @PathVariable Long id,
+            @RequestBody @Valid CategoryDTORequest request
+    ){
+        request.setId(id);
+        Category cat = service.update(this.mapper.map(request, Category.class));
+
+        CategoryDTOResponse response = this.mapper.map(cat, CategoryDTOResponse.class);
+
+        return ResponseEntity.ok().body(response);
+    }
+
     @PutMapping("/deactivate/{id}")
-    public ResponseEntity<String> deactivateCateogry(@PathVariable Long id){
+    public ResponseEntity<String> deactivateCategory(@PathVariable Long id){
         Category cat = service.findById(id);
         service.deactivate(cat);
         return ResponseEntity.status(HttpStatus.OK).body("Categoria desativada");
